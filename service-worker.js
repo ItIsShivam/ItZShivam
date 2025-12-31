d053da35b2b04280070c97980e9afa7f566d02fb
Shivam <devsparadise999@duck.com>
2025-12-31 20:51:48 +0530

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = "shivam-music-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll([
+        "./",
+        "ItIsShivam.html",
+        "ItIsShivam.css",
+        "ItIsShivam.js"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
